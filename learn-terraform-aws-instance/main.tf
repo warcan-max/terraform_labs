8d6a9f1f30f9ba359cf5a64ae40109c7c2ef67d7
warcan <warcan@mivanov-217.73.86.191-lesson12>
2022-06-16 00:01:52 +0300

    terraform_labs

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "warcan_org"
+    workspaces {
+      name = "Test-Workspace"
+    }
+ }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-08d70e59c07c61a3a"
+  #ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
